42ad2bf8e2bda34f4abf425f22d0a0635916c2d3
James Bayliss <l3uddz@gmail.com>
2021-03-21 13:47:07 +0000

    resolve issue with time locations
    times from smoothstream guide is in EDT / New York time

--- smoothstreams/util/time.go
+++ smoothstreams/util/time.go
@@ -11,9 +11,9 @@ var (
 
 func init() {
 	// time locations
-	loc, err := time.LoadLocation("EST")
+	loc, err := time.LoadLocation("America/New_York")
 	if err != nil {
-		panic(fmt.Sprintf("load est time location: %v", err))
+		panic(fmt.Sprintf("load time location: %v", err))
 	}
 	locSmoothStreams = loc
 }
@@ -24,11 +24,11 @@ func CurrentXmlTvTime() string {
 }
 
 func TimeStringToXmlTvTime(ts string) (string, error) {
-	// parse time from smoothstreams (comes in EST)
+	// parse time from smoothstreams (comes in EST/EDT - New York)
 	t, err := time.ParseInLocation("2006-01-02 15:04:05 EST", ts+" EST", locSmoothStreams)
 	if err != nil {
 		return "", fmt.Errorf("parse in location: %v: %w", ts, err)
 	}
 
-	return t.Format("20060102150405 -0700"), nil
+	return t.UTC().Format("20060102150405 -0700"), nil
 }
