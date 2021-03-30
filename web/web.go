0de8ed75e28af482ad3673d7d2fab08f78dd8edf
l3uddz <l3uddz@gmail.com>
2021-03-30 18:21:55 +0100

    web: temporary cache of epg (#6)

--- web/web.go
+++ web/web.go
@@ -2,10 +2,13 @@ package web
 
 import (
 	"github.com/dustin/go-humanize"
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 	"github.com/l3uddz/sstv/logger"
 	"github.com/l3uddz/sstv/smoothstreams"
 	"github.com/rs/zerolog"
+	"golang.org/x/sync/singleflight"
 	"strings"
 	"time"
 )
@@ -14,7 +17,9 @@ type Client struct {
 	ss         *smoothstreams.Client
 	forceProxy bool
 
-	log zerolog.Logger
+	store *persistence.InMemoryStore
+	sfg   *singleflight.Group
+	log   zerolog.Logger
 }
 
 func New(ss *smoothstreams.Client, forceProxy bool) *Client {
@@ -22,7 +27,9 @@ func New(ss *smoothstreams.Client, forceProxy bool) *Client {
 		ss:         ss,
 		forceProxy: forceProxy,
 
-		log: logger.New(""),
+		store: persistence.NewInMemoryStore(time.Second),
+		sfg:   &singleflight.Group{},
+		log:   logger.New(""),
 	}
 }
 
@@ -30,7 +37,7 @@ func (c *Client) SetHandlers(r *gin.Engine) {
 	// core
 	r.GET("/playlist.m3u8", c.Playlist)
 	r.GET("/stream/:channel", c.WithSanitizedRawQuery(c.Stream))
-	r.GET("/epg.xml", c.EPG)
+	r.GET("/epg.xml", cache.CachePage(c.store, time.Minute, c.EPG))
 	// plex
 	r.GET("/lineup.json", c.Lineup)
 	r.GET("/lineup_status.json", c.LineupStatus)
@@ -43,11 +50,6 @@ func (c *Client) SetHandlers(r *gin.Engine) {
 
 func (c Client) WithSanitizedRawQuery(next func(*gin.Context)) gin.HandlerFunc {
 	return func(g *gin.Context) {
-		if g.Request == nil {
-			next(g)
-			return
-		}
-
 		// plex dvr appends its transcode query param with a ?, even if the url already contains a question mark
 		// this causes issues when it comes to decoding, and thus, this crude fix is required
 		if strings.Contains(g.Request.URL.RawQuery, "?") {
@@ -60,11 +62,6 @@ func (c Client) WithSanitizedRawQuery(next func(*gin.Context)) gin.HandlerFunc {
 
 func (c *Client) Logger() gin.HandlerFunc {
 	return func(g *gin.Context) {
-		if g.Request == nil {
-			g.Next()
-			return
-		}
-
 		// log request
 		rl := c.log.With().
 			Str("ip", g.ClientIP()).
